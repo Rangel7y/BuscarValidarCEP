e4edbcb422fc8512d2362f1c03144f78ac80c163
f4t4l777 <rangel0011@outlook.com>
2023-04-15 09:15:36 -0300

    Updated

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 
+//-- GET DOCUMENT ELEMENTS  --//
+//-- FORM CONTENT --//
+const formContent = document.getElementById("form-content");
+const inputSearchCep = document.getElementById("input-search-cep");
+//
 
+//-- DEFAULT DATA  --//
 const appCepId = '45993135';
+//
 
+//-- FUNCTION TO GET API DATA --//
 const findCEP = async(cepId) =>{
     const APIResponse = await fetch(`https://viacep.com.br/ws/${cepId}/json/`);
 
@@ -10,7 +18,9 @@ const findCEP = async(cepId) =>{
         return dataCep;
     }
 }
+//
 
+//-- FUNCTION CALL FUNCTION GET API DATA --//
 const getCEP = async(cepId) =>{
     const dataCep = await findCEP(cepId);
 
@@ -19,5 +29,15 @@ const getCEP = async(cepId) =>{
         console.log(dataCep);
     }
 }
+//
 
-getCEP(appCepId);
+//-- BUTTON INPUT FORM --//
+formContent.addEventListener('submit',(event) =>{
+    event.preventDefault();
+    if(inputSearchCep.value.length != 0){
+        getCEP(inputSearchCep.value);
+    }
+    else{
+        console.error("Digite o número do CEP para buscar!");
+    }
+});
