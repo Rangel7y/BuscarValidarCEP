dad05430b78aa46927ae191cbde146b0900179d1
f4t4l777 <rangel0011@outlook.com>
2023-07-11 11:55:53 -0300

    Updated

---
+++ js/consultAddress.js
@@ -0,0 +1,56 @@
+
+
+
+
+/* GET PANEL FORM TO FILL INFOS */
+const form2Content = document.getElementById('form2-content');
+const inputFillAll = document.querySelectorAll('.input-find-cep');
+const inputFillUf = document.querySelector('.input-fill-uf');
+const inputFillCidade = document.querySelector('.input-fill-cidade');
+const inputFillBairro = document.querySelector('.input-fill-bairro');
+const inputFillRua = document.querySelector('.input-fill-rua');
+/* --- --- */
+
+
+
+
+/* GET API FILLED INFOS */
+const getAPIFilled = async(uf,cidade,rua) =>{
+    const APIResponse = await fetch(`https://viacep.com.br/ws/${uf}/${cidade}/${rua}/json/`);
+
+    if(APIResponse.status == 200){
+        const data = await APIResponse.json();
+        return data;
+    }
+}
+const getInfoFilled = async(uf,cidade,rua) =>{
+    const data = await getAPIFilled(uf,cidade,rua);
+
+    if(!("erro" in data)){
+        showBoxResult(true);
+
+        resultCep01.textContent = data['0']['cep'];
+        //
+        resultCep02.textContent = data['0']['ddd'];
+        //
+        resultCep03.textContent = data['0']['localidade'];
+        //
+        resultCep04.textContent = data['0']['uf'];
+        //
+        resultCep05.textContent = data['0']['bairro'];
+        //
+        resultCep06.textContent = data['0']['logradouro'];    
+        
+        console.log(data);
+    }
+    else{
+        console.error("Erro ao procurar pelo CEP fornecido!");
+    }
+}
+
+form2Content.addEventListener('submit',(event) =>{
+    event.preventDefault();
+
+    getInfoFilled(inputFillUf.value,inputFillCidade.value,inputFillRua.value); 
+});
+/* --- --- */
--- js/consultCEP.js
+++ js/consultCEP.js
@@ -15,14 +15,7 @@ const resultCep06 = document.getElementById("resultCep-06");
 const boxResultCep = document.getElementById("panel-result-container");
 //
 
-/* GET PANEL FORM TO FILL INFOS */
-const form2Content = document.getElementById('form2-content');
-const inputFillAll = document.querySelectorAll('.input-find-cep');
-const inputFillUf = document.querySelector('.input-fill-uf');
-const inputFillCidade = document.querySelector('.input-fill-cidade');
-const inputFillBairro = document.querySelector('.input-fill-bairro');
-const inputFillRua = document.querySelector('.input-fill-rua');
-//
+
 
 //-- DEFAULT DATA  --//
 const appCepId = '45993135';
@@ -87,47 +80,6 @@ form1Content.addEventListener('submit',(event) =>{
 
         console.error("Digite o número do CEP para buscar!");
     }
-});
-//
-
-/* GET API FILLED INFOS */
-const getAPIFilled = async(uf,cidade,rua) =>{
-    const APIResponse = await fetch(`https://viacep.com.br/ws/${uf}/${cidade}/${rua}/json/`);
-
-    if(APIResponse.status == 200){
-        const data = await APIResponse.json();
-        return data;
-    }
-}
-const getInfoFilled = async(uf,cidade,rua) =>{
-    const data = await getAPIFilled(uf,cidade,rua);
-
-    if(!("erro" in data)){
-        showBoxResult(true);
-
-        resultCep01.textContent = data['0']['cep'];
-        //
-        resultCep02.textContent = data['0']['ddd'];
-        //
-        resultCep03.textContent = data['0']['localidade'];
-        //
-        resultCep04.textContent = data['0']['uf'];
-        //
-        resultCep05.textContent = data['0']['bairro'];
-        //
-        resultCep06.textContent = data['0']['logradouro'];    
-        
-        console.log(data);
-    }
-    else{
-        console.error("Erro ao procurar pelo CEP fornecido!");
-    }
-}
-
-form2Content.addEventListener('submit',(event) =>{
-    event.preventDefault();
-
-    getInfoFilled(inputFillUf.value,inputFillCidade.value,inputFillRua.value); 
 });
 //
 
