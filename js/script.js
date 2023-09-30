1ce51806342efa3105df23f7ee1454d94c019a1a
f4t4l777 <rangel0011@outlook.com>
2023-09-30 15:20:48 -0300

    feat: added new title on secTxt2FrmChk

--- js/script.js
+++ js/script.js
@@ -392,7 +392,7 @@ function UpdateTxtPg(){
 
             secTxt1FrmChk.innerText = "Nossa ferramenta 'Validar CEP' permite que você verifique a validade e existência de um CEP no sistema postal, além de fornecer informações detalhadas sobre o endereço correspondente. É uma solução eficiente para garantir a precisão dos dados, essencial em e-commerce e logística.";
 
-            secTxt2FrmChk.innerHTML = `Caso queira voltar ao modo de 'Validar CEP', <a href="#" onclick="SwMd()">clique aqui</a> para fazer a troca.`;
+            secTxt2FrmChk.innerHTML = `Caso queira voltar ao modo de 'Validar CEP', <a href="#" onclick="SwMd()" title="Alternar para modo consulta de ENDEREÇO">clique aqui</a> para fazer a troca.`;
 
             break;
         case typeSwFrmMd.typeFrmChkAddress:
@@ -400,7 +400,7 @@ function UpdateTxtPg(){
 
             secTxt1FrmChk.innerText = "Nossa ferramenta 'Consultar Endereço' permite que você obtenha informações detalhadas de um endereço informando o Estado (UF), Cidade e nome da Rua. É uma maneira rápida e eficaz de acessar dados precisos sobre o local desejado, útil em várias situações cotidianas.";
 
-            secTxt2FrmChk.innerHTML = `Caso queira voltar ao modo de 'Consultar Endereço', <a href="#" onclick="SwMd()">clique aqui</a> para fazer a troca.`;
+            secTxt2FrmChk.innerHTML = `Caso queira voltar ao modo de 'Consultar Endereço', <a href="#" onclick="SwMd()" title="Alternar para modo validação de CEP">clique aqui</a> para fazer a troca.`;
             break;
         default:
             break;
